91d43e12f7bd1f9cc8fd608adc3dd15d72fdd260
2qi <46019382+2qi@users.noreply.github.com>
2019-07-21 18:26:57 -0700

    vs code test

---
+++ sample.c
@@ -0,0 +1,35 @@
+/*
+ * A simple c code to add 2 numbers
+ */
+
+#include <stdio.h>
+#include <ctype.h>
+#define NUM1 20
+#define NUM2 10
+
+int main(){
+    int32_t num1 =NUM1, num2 =NUM2;
+
+    // enter 2 numbers from console - user
+    printf("Enter first number (press return to use default val of 20): ");
+    scanf("%d", &num1);
+    printf("Enter second number (press return to use default val of 10): ");
+    scanf("%d", &num2);
+
+     if(!(isdigit(num1))){
+        num1 = NUM1;
+        printf("You aint a good boy/girl! Defaulting the value for number 1");
+     }
+    // if(!(isnum(num2)){
+    //     num2 = NUM2;
+    //     printf("You aint a good boy/girl! Defaulting the value for number 1");
+    // }
+
+    int32_t sum = num1 + num2;
+
+    printf("The sum of the two numbers is : %d + %d = %d", num1, num2, sum);
+
+//    print
+
+   char cg = getchar();
+}
